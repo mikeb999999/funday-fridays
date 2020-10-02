b5d3e851fff55dec84b5abb9ebb36edcb9a2611d
Mike B <mikeb999999@gmail.com>
2020-10-02 16:56:15 +0100

    Add pt04 : introduction-to-components, & start my own project AudioAng0

--- AudioAng0/src/app/app.module.ts
+++ AudioAng0/src/app/app.module.ts
@@ -1,16 +1,21 @@
+// THE ANGULAR MODULE!!!  This is the starting point
+
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//The @NgModule decorator - contains arrays - defines this class as an angular module
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent   /* all external components */
   ],
   imports: [
-    BrowserModule
+    BrowserModule   /*s.t. app runs correctly in the browser */
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent  /* the starting componen for the app.*/ /*contains the directive used in the html file */ ]
 })
+// The class, obviously
 export class AppModule { }
--- README.md
+++ README.md
@@ -5,3 +5,8 @@ T0821 - adding web app for experimental purposes (developed in 2019 thru PS cour
 
 + try pull request
 + add my Razor web app code
+
+............................
+
+T0928 - adding in APM - Ang getting Started app.   starts at pt 03
+
